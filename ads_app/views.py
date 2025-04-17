53d08ba714caada5fb6ce14685437ad8b455145d
ArturStepanyan10 <stepanyanartur110@gmail.com>
2025-04-17 17:38:32 +0300

    feat(ExchangeProposal) and feat(User): Added viewing offers, status updates, and authorization.

--- ads_app/views.py
+++ ads_app/views.py
@@ -1,13 +1,16 @@
+from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponseForbidden, Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-from ads_app.forms import AddAdForm, ExchangeOfferForm
-from ads_app.models import Ad
+from ads_app.forms import AddAdForm, ExchangeOfferForm, LoginUserForm
+from ads_app.models import Ad, ExchangeProposal
 
 
-class Home(ListView):
+class Home(LoginRequiredMixin, ListView):
     model = Ad
     template_name = 'ads/index.html'
     title_page = 'Главная страница'
@@ -22,7 +25,7 @@ class Home(ListView):
         return Ad.objects.exclude(user=self.request.user)
 
 
-class MyAds(ListView):
+class MyAds(LoginRequiredMixin, ListView):
     model = Ad
     template_name = 'ads/myAds.html'
     title_page = 'Мои объявления'
@@ -37,7 +40,7 @@ class MyAds(ListView):
         return Ad.objects.filter(user=self.request.user)
 
 
-class AddAd(CreateView):
+class AddAd(LoginRequiredMixin, CreateView):
     form_class = AddAdForm
     template_name = 'ads/AddAd.html'
     title_page = 'Создание объявления'
@@ -52,7 +55,7 @@ class AddAd(CreateView):
         return super().form_valid(form)
 
 
-class UpdateAd(UpdateView):
+class UpdateAd(UpdateView): #PermissionRequiredMixin
     model = Ad
     fields = ['title', 'description', 'image_url', 'category', 'condition']
     template_name = 'ads/AddAd.html'
@@ -63,7 +66,7 @@ class UpdateAd(UpdateView):
     }
 
 
-class DeleteAd(DeleteView):
+class DeleteAd(LoginRequiredMixin, DeleteView):
     model = Ad
     success_url = reverse_lazy("my_ads")
 
@@ -74,7 +77,7 @@ class DeleteAd(DeleteView):
         return ad
 
 
-class ExchangeOffer(CreateView):
+class ExchangeOffer(LoginRequiredMixin, CreateView):
     form_class = ExchangeOfferForm
     template_name = 'ads/ExchangeОffer.html'
     title_page = 'Предложение обмена'
@@ -93,3 +96,56 @@ class ExchangeOffer(CreateView):
         ad_receiver = get_object_or_404(Ad, pk=self.kwargs.get('pk'))
         ep.ad_receiver = ad_receiver
         return super().form_valid(form)
+
+
+class ExchangeOfferList(LoginRequiredMixin, ListView):
+    model = ExchangeProposal
+    template_name = 'ads/ExchangeProposal.html'
+    title_page = 'Предложения на обмен'
+    context_object_name = 'offers'
+    extra_context = {
+        'title': title_page,
+    }
+
+    def get_queryset(self):
+        return ExchangeProposal.objects.filter(ad_receiver__user=self.request.user)
+
+
+def offer_action(request, pk):
+    if request.method == 'POST':
+        offer = get_object_or_404(ExchangeProposal, pk=pk)
+
+        action = request.POST['action']
+        if action == 'accept':
+            offer.status = offer.Status.ACCEPTED
+        elif action == 'reject':
+            offer.status = offer.Status.REJECTED
+        offer.save()
+
+    return redirect('list_offers')
+
+
+class MyExchangeProposalsList(LoginRequiredMixin, ListView):
+    model = ExchangeProposal
+    template_name = 'ads/MyExchangeProposal.html'
+    title_page = 'Мои предложения на обмен'
+    context_object_name = 'offers'
+    extra_context = {
+        'title': title_page,
+    }
+
+    def get_queryset(self):
+        return ExchangeProposal.objects.filter(ad_sender__user=self.request.user)
+
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'ads/Authorization.html'
+    extra_context = {
+        'title': 'Авторизация'
+    }
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
