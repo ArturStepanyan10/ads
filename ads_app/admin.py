3e9dd23d6109d5e79dc38d6f2721066d17d8e51e
ArturStepanyan10 <stepanyanartur110@gmail.com>
2025-04-18 20:59:21 +0300

    The administration panel is configured

--- ads_app/admin.py
+++ ads_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from ads_app.models import Ad, ExchangeProposal
+
+
+@admin.register(Ad)
+class ModelNameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'image_url', 'condition', 'category', 'user')
+    list_display_links = ('title', )
+
+
+@admin.register(ExchangeProposal)
+class ExchangeProposalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ad_receiver', 'ad_sender', 'comment', 'status')
